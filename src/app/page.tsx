f0a5ddccc3dd3305fff990baa0d953079f057367
Richard-S16 <richard_s16@protonmail.com>
2023-10-15 17:27:07 -0300

    Finish Home Page mobile version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -38,7 +38,7 @@ export default function Home() {
       <div className="flex justify-center">
         <button className="hover:shadow-exploreButton mt-20 h-[150px] w-[150px] rounded-full bg-color-3 font-Bellefair text-xl tracking-[1.25px] text-color-1 transition-shadow">
           EXPLORE
-        </button
+        </button>
       </div>
     </div>
   );
